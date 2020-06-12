f78377179c7c3e785f96b6586b7c840a52a5c2c0
SophiaNunner <sophi.nunner@gmail.com>
2020-06-12 09:42:53 +0200

    Main simplified for Buddy testing purposes

--- main/main.cpp
+++ main/main.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 int main(){
 
-    Hangman hm;
-    hm.playGame("continuous delivery");
+    //Hangman hm;
+    //hm.playGame("continuous delivery");
 
     return EXIT_SUCCESS;
 }
